23d198a17645d4731779ba46e327f09d332b819c
Daniel Garcia Moreno <danigm@wadobo.com>
2012-05-18 09:37:12 +0200

    Changing menu texts

--- etruekko/truekko/utils.py
+++ etruekko/truekko/utils.py
@@ -3,13 +3,13 @@ from django.core.urlresolvers import reverse
 
 def generate_menu(selected=''):
     menu = (
-        ("home", _("Home"), reverse("index"), selected == "home"),
-        ("people", _("People"), reverse("people"), selected == "people"),
+        ("home", _("Wall"), reverse("index"), selected == "home"),
+        ("people", _("My contacts"), reverse("people"), selected == "people"),
         ("serv", _("Services"), reverse("item_list", args=('serv',)), selected == "serv"),
         ("item", _("Item"), reverse("item_list", args=('item',)), selected == "item"),
-        ("add", _("Add"), reverse("item_add"), selected == "add"),
-        ("transf", _("Transf"), reverse("transfer_list"), selected == "transf"),
-        ("group", _("Groups"), reverse("groups"), selected == "group"),
+        ("add", _("Create your offerts and demands"), reverse("item_add"), selected == "add"),
+        ("transf", _("My swaps"), reverse("transfer_list"), selected == "transf"),
+        ("group", _("My groups"), reverse("groups"), selected == "group"),
     )
 
     return menu
